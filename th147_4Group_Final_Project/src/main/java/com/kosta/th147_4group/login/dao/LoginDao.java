7baee8d76d50595619087b65ce3c035c96beec21
Hyunaoh <sarah718@naver.com>
2017-06-19 22:39:40 +0900

    Update LoginDao.java
    살짝 바꿨습니다.

--- th147_4Group_Final_Project/src/main/java/com/kosta/th147_4group/login/dao/LoginDao.java
+++ th147_4Group_Final_Project/src/main/java/com/kosta/th147_4group/login/dao/LoginDao.java
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 package com.kosta.th147_4group.login.dao;
 
 public interface LoginDao {
 
 }
-=======
-package com.kosta.th147_4group.login.dao;
 
-public interface LoginDao {
-
-}
->>>>>>> ae2ad45f8e7d51eddcfa24cddc9ce44acd071cc9
